1b1f3a21dd3e78b783d3d4adec6f8d7feb322020
AdilzaMattos <adilzamattos@gmail.com>
2019-08-07 15:44:25 -0300

    Projeto Java
    MarvelJava Inicial

---
+++ automacaojava/src/main/java/automacaojava/App.java
@@ -0,0 +1,13 @@
+package automacaojava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ automacaojava/src/main/java/automacaojava/Web.java
@@ -0,0 +1,12 @@
+package automacaojava;
+
+/**
+ * @author Adilza Mattos
+ *  Automação de Teste com Java.
+ */
+public class Web {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+}
